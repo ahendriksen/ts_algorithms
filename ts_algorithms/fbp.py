Fix scaling of fbp
First of all: thanks to Dirk Schut for investigating this issue and
finding this fix!

The scaling performed in the fbp function was off for various
non-standard detector pixel sizes. This has now been fixed.

The following code was used to test this function. It is difficult to
integrate this into the tests, as fbp reconstructions are affected by
discretisation errors when the pixel size becomes too large.

------------------------------------------------------------

import numpy as np
import torch
import tomosipo as ts
from ts_algorithms import fbp

vol_shape = 64
det_shape = np.array([vol_shape * 2, vol_shape * 2])
angles = vol_shape * 3

pix_sizes = np.array([
    [1.0, 1.0],
    [1.0, 2.0],
    [2.0, 1.0],
    [2.0, 2.0],
    [2.0, 3.0],
    [3.0, 2.0],
    [3.0, 3.0],
    [3.0, 4.0],
    [4.0, 3.0],
    [4.0, 4.0],
])
scaling_factors = [0.5, 1.0, 2.0, 4.0]

error_factors = np.zeros((len(pix_sizes), len(scaling_factors)))

for i, det_pix_size in enumerate(pix_sizes):

    pg = ts.parallel(
        angles=angles,
        size=det_pix_size * det_shape,
        shape=det_shape,
    ).to_vec()

    vg = ts.volume(size=vol_shape, shape=vol_shape)
    x = torch.ones(vg.shape)

    for j, s in enumerate(scaling_factors):
        S = ts.scale(s)
        A = ts.operator(S * vg, S * pg)

        y = A(x)

        r = fbp(A, y)

        error_factors[i, j] = r.mean() / x.mean()

        print(f"det_pix_size: {det_pix_size} s: {s:0.2f} in_vol mean = {x.mean()}, reconstruction mean = {r.mean()}")

------------------------------------------------------------

Output:

------------------------------------------------------------

det_pix_size: [1. 1.] s: 0.50 in_vol mean = 1.0, reconstruction mean = 0.9927359223365784
det_pix_size: [1. 1.] s: 1.00 in_vol mean = 1.0, reconstruction mean = 0.9927359223365784
det_pix_size: [1. 1.] s: 2.00 in_vol mean = 1.0, reconstruction mean = 0.9927359223365784
det_pix_size: [1. 1.] s: 4.00 in_vol mean = 1.0, reconstructio

ts_algorithms/fbp.py
@@ -157,10 +157,16 @@ def fbp(A, y, padded=True, filter=None, reject_acyclic_filter=True):
     # Scale result to make sure that fbp(A, A(x)) == x holds at least
     # to some approximation. In limited experiments, this is true for
     # this version of FBP up to 1%.
+    # *Note*: For some reason, we do not have to scale with respect to
+    # the pixel dimension that is orthogonal to the rotation axis (`u`
+    # or horizontal pixel dimension). Hence, we only scale with the
+    # other pixel dimension (`v` or vertical pixel dimension).
     vg, pg = A.volume_geometry, A.projection_geometry
-    pixel_area = np.prod(np.array(pg.det_size) / np.array(pg.det_shape))
+
+    pixel_height = (pg.det_size[0] / pg.det_shape[0])
     voxel_volume = np.prod(np.array(vg.size / np.array(vg.shape)))
-    scaling = (np.pi / pg.num_angles) * (pixel_area / voxel_volume) ** 2
+    scaling = (np.pi / pg.num_angles) * pixel_height / voxel_volume
+
     rec *= scaling
 
     return rec