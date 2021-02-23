71dc484079dc3d89c7741cd69862a5ccedc2598b
D1rk123 <dirk.schut@cwi.nl>
2021-02-23 18:56:09 +0100

    fdk: Address review comments

--- tests/test_fdk.py
+++ tests/test_fdk.py
@@ -2,44 +2,92 @@ import pytest
 import torch
 import tomosipo as ts
 from ts_algorithms import fdk
+from matplotlib import pyplot as plt
 
 
 def make_box_phantom():
-    x = torch.ones((32, 32, 32))
-    x[4:28, 4:28, 4:28] = 1.0
-    x[12:22, 12:22, 12:22] = 0.0
+    x = torch.zeros((64, 64, 64))
+    x[8:56, 8:56, 8:56] = 1.0
+    x[24:44, 24:44, 24:44] = 0.0
     return x
 
 
 def test_fdk_reconstruction():
-    vg = ts.volume(shape=32, size=1)
-    pg = ts.cone(angles=32, shape=(48, 48), size=(2, 2), src_det_dist=2, src_orig_dist=2)
+    vg = ts.volume(shape=64, size=1)
+    pg = ts.cone(angles=32, shape=(64, 64), size=(2, 2), src_det_dist=2, src_orig_dist=2)
 
     A = ts.operator(vg, pg)
     x = make_box_phantom()
     y = A(x)
 
-    # very rough test if reconstruction is close to original volume
+    # rough test if reconstruction is close to original volume
     rec = fdk(A, y)
-    assert torch.allclose(rec, x, atol=1, rtol=0)
+    assert torch.mean(torch.abs(rec - x)) < 0.15
     rec_nonPadded = fdk(A, y, padded=False)
-    assert torch.allclose(rec_nonPadded, x, atol=1, rtol=0)
-
-    # for a circular geometry recalculating the weights should not change the result
-    rec_recalculate = fdk(A, y, recalculate_weights=True)
-    assert torch.allclose(rec_recalculate, rec, atol=1e-6, rtol=1e-5)
+    assert torch.mean(torch.abs(rec_nonPadded - x)) < 0.15
 
     # test whether GPU and CPU calculations yield the same result
-    assert torch.allclose(fdk(A, y.cuda()).cpu(), fdk(A, y.cpu()), atol=1e-4, rtol=1e-3)
+    rec_cuda = fdk(A, y.cuda()).cpu()
+    assert torch.allclose(rec_cuda, rec, atol=1e-3, rtol=1e-2)
+    assert torch.mean(torch.abs(rec_cuda - rec)) < 1e-6
 
 
 def test_fdk_anisotropic_pixels():
-    vg = ts.volume(shape=32, size=1)
-    pg = ts.cone(angles=32, shape=(48, 64), size=(2, 2), src_det_dist=2, src_orig_dist=2)
+    vg = ts.volume(shape=64, size=1)
+    pg = ts.cone(angles=32, shape=(64, 80), size=(2, 2), src_det_dist=2, src_orig_dist=2)
 
     A = ts.operator(vg, pg)
     x = make_box_phantom()
     y = A(x)
 
-    # very rough test for reconstruction with anisotropic detector pixels
-    assert torch.allclose(fdk(A, y), x, atol=1, rtol=0)
+    # rough test for reconstruction with anisotropic detector pixels
+    rec = fdk(A, y)
+    assert torch.mean(torch.abs(rec - x)) < 0.15
+
+
+def test_fdk_translation_invariance():
+    vg = ts.volume(shape=64, size=1)
+    pg = ts.cone(angles=32, shape=(64, 64), size=(2, 2), src_det_dist=2, src_orig_dist=2).to_vec()
+
+    # Create sinogram and reconstruct at the origin
+    A0 = ts.operator(vg, pg)
+    x = make_box_phantom()
+    y = A0(x)
+    rec0 = fdk(A0, y, src_rot_center_dist=2)
+    bp0 = A0.T(y)
+
+    # Move volume and detector far away
+    T = ts.translate((1000, 1000, 1000))
+    A = ts.operator(T * vg, T * pg)
+
+    rec = fdk(A, y, src_rot_center_dist=2)
+    bp = A.T(y)
+
+    # Check that backprojection still matches
+    assert torch.allclose(bp, bp0)
+    # Check that fdk still matches
+    assert torch.allclose(rec, rec0)
+
+ 
+def test_fdk_scaling_invariance():
+    vg = ts.volume(shape=64, size=1)
+    pg = ts.cone(angles=32, shape=(64, 64), size=(2, 2), src_det_dist=2, src_orig_dist=2).to_vec()
+
+    # Create sinogram and reconstruct at the origin
+    A0 = ts.operator(vg, pg)
+    x = make_box_phantom()
+
+    rec0 = fdk(A0, A0(x), src_rot_center_dist=2)
+    bp0 = A0.T(A0(x))
+
+    s = 2.0
+    S = ts.scale(s)
+    A = ts.operator(S * vg, S * pg)
+
+    rec = fdk(A, A(x), src_rot_center_dist=2*s)
+    bp = A.T(A(x))
+
+    # Check that backprojection still matches (up to scaling)
+    assert torch.allclose(bp / s**2, bp0)
+    # Check that fdk still matches
+    assert torch.allclose(rec, rec0)
--- ts_algorithms/fdk.py
+++ ts_algorithms/fdk.py
@@ -4,75 +4,105 @@ import tomosipo as ts
 from .fbp import fbp
 
 
-def fdk_weigh_projections(op, projections, recalculate_weights):
-    dev = projections.device
-    u_num_pixels = op.range_shape[2]
-    u_space = (torch.arange(u_num_pixels, dtype=torch.float, device=dev)+0.5-(u_num_pixels/2))
-    v_num_pixels = op.range_shape[0]
-    v_space = (torch.arange(v_num_pixels, dtype=torch.float, device=dev)+0.5-(v_num_pixels/2))
+def fdk_weigh_projections(op, projections, src_rot_center_dist):
+    # The distance of each pixel to the source is calulated in a coordinate
+    # system based on the normalized u, v and normal vectors of the detector
+    # because that way the distance along the v axis is constant along each
+    # row and the distance along the u axis is constant along each column
+    # resulting in fewer computations
+    
+    src_to_det = torch.from_numpy(op.range.det_pos[0] - op.range.src_pos[0])
+    src_det_dist = torch.norm(src_to_det)
+    # Load u, v vectors:
+    det_u = torch.from_numpy(op.range.det_u[0])
+    det_v = torch.from_numpy(op.range.det_v[0])
+    # Calculate u, v vector lengths:
+    det_u_norm = torch.norm(det_u)
+    det_v_norm = torch.norm(det_v)
+    # Transform source to detector center vector to the u, v system
+    src_det_u_dist = torch.dot(src_to_det, det_u) / det_u_norm
+    src_det_v_dist = torch.dot(src_to_det, det_v) / det_v_norm
 
-    result = torch.empty(projections.shape, dtype=torch.float, device=dev)
+    #calculate the distance of each row and column of pixels along the u or v axis
+    u_num_pixels = op.range_shape[2]
+    u_space = (torch.arange(u_num_pixels, dtype=torch.float64)+0.5-(u_num_pixels/2))
+    v_num_pixels = op.range_shape[0]
+    v_space = (torch.arange(v_num_pixels, dtype=torch.float64)+0.5-(v_num_pixels/2))    
+    
+    # Calculate the minimal source to detector distance divided by the
+    # pixel to detector distance for each pixel. The pixel to detector 
+    # distances are calculated using the pythagorean theorem
+    u_pos_squared = (u_space * det_u_norm + src_det_u_dist)**2
+    v_pos_squared = (v_space * det_v_norm + src_det_v_dist)**2
+
+    weights_mat = src_det_dist / torch.sqrt(
+          u_pos_squared[None, :].expand([v_num_pixels, u_num_pixels])
+          + v_pos_squared[:, None].expand([v_num_pixels, u_num_pixels])
+          + src_det_dist**2
+    )
+    
+    # Multiply with extra scaling factor to account for detector distance
+    weights_mat *= (src_rot_center_dist / src_det_dist)
+    weights_mat = weights_mat.float().to(projections.device)
+
+    result = projections.new_empty(projections.shape).to(projections.device)
 
     for i in range(op.range_shape[1]):
-        if recalculate_weights or i == 0:
-            src_to_det = torch.from_numpy(op.range.det_pos[i] - op.range.src_pos[i]).float().to(dev)
-            src_to_obj = torch.from_numpy(-op.range.src_pos[i]).float().to(dev)
-            det_n = torch.from_numpy(op.range.det_normal[i]).float().to(dev)
-            det_u = torch.from_numpy(op.range.det_u[i]).float().to(dev)
-            det_v = torch.from_numpy(op.range.det_v[i]).float().to(dev)
-            det_n_norm = torch.norm(det_n)
-            det_u_norm = torch.norm(det_u)
-            det_v_norm = torch.norm(det_v)
-            src_det_n_dist = torch.dot(src_to_det, det_n) / det_n_norm
-            src_det_u_dist = torch.dot(src_to_det, det_u) / det_u_norm
-            src_det_v_dist = torch.dot(src_to_det, det_v) / det_v_norm
-            src_to_obj_n_dist = torch.dot(src_to_obj, det_n) / det_n_norm
-
-            u_pos_squared = (u_space * det_u_norm + src_det_u_dist)**2
-            v_pos_squared = (v_space * det_v_norm + src_det_v_dist)**2
-
-            weights_mat = src_det_n_dist / torch.sqrt(
-                  u_pos_squared[None, :].expand([v_num_pixels, u_num_pixels])
-                  + v_pos_squared[:, None].expand([v_num_pixels, u_num_pixels])
-                  + src_det_n_dist**2
-            )
-            # scale detector to origin
-            weights_mat *= (src_to_obj_n_dist / src_det_n_dist)
-
         result[:, i, :] = projections[:, i, :] * weights_mat
 
     return result
 
 
-def fdk(A, y, padded=True, filter=None, reject_acyclic_filter=True, recalculate_weights=False):
+def fdk(A, y, padded=True, filter=None, reject_acyclic_filter=True, src_rot_center_dist=None):
     """Approximately reconstruct volumes in a circular cone beam geometry using
-    the Feldkamp, Davis and Kress(FDK) algorithm
+    the Feldkamp, Davis and Kress(FDK) algorithm [1]. Arbtrary shifts, scaling
+    and rotations along the vertical axis are also supported.
 
     If `y` is located on GPU, the entire algorithm is executed on a single GPU.
 
     If `y` is located in RAM (CPU in PyTorch parlance), then only the
     foward and backprojection are executed on GPU.
 
-    :param A: `tomosipo.operator`
-    :param y: `torch.tensor`
+    :param A: `tomosipo.operator` The projection geometry of the operator must
+    be either cone or cone_vec. If it is cone_vec parameter src_rot_center_dist
+    must be provided too
+    :param y: `torch.tensor` sinogram stack with the following layout:
+    (num_vertical_pixels, num_angles, num_horizontal_pixels)
     :param padded: bool, is passed to ts_algorithms.fbp
     :param filter: bool, is passed to ts_algorithms.fbp
     :param reject_acyclic_filter: bool, is passed to ts_algorithms.fbp
-    :param recalculate_weights: bool, experimental feature: recalculate FDK weights
-    every angle when using non-circular geometries
+    :param src_rot_center_dist: the distance from the source to the
+    center of rotation of the object. Only used and required when the
+    projection geometry is cone_vec
     :returns: reconstruction of a volume
     :rtype: `torch.tensor`
-
+    
+    [1] Feldkamp, L. A., Davis, L. C., & Kress, J. W. (1984). Practical Cone-Beam
+    Algorithm. Journal of the Optical Society of America A, 1(6), 612.
+    http://dx.doi.org/10.1364/josaa.1.000612
     """
     voxel_sizes = np.array(A.volume_geometry.size) / np.array(A.volume_geometry.shape)
-    if (np.ptp(voxel_sizes) > ts.epsilon):
+    if np.ptp(voxel_sizes) > ts.epsilon:
         raise ValueError(
             "The voxels in the volume are required to have the same size in every dimension.\n"
             f"This is not the case: voxel size = {voxel_sizes}."
         )
+    pg = A.range
+    if isinstance(pg, ts.geometry.cone_vec.ConeVectorGeometry):
+        if src_rot_center_dist is None:
+            raise ValueError(
+                "When pg is a cone vector geometry parameter src_rot_center_dist needs to be provided"
+            )
+    elif isinstance(pg, ts.geometry.cone.ConeGeometry):
+        src_rot_center_dist = pg.src_orig_dist
+    else:
+        raise ValueError(
+            "The provided operator A needs to describe a cone beam geometry."
+        )
+        
     return fbp(
         A=A,
-        y=fdk_weigh_projections(A, y, recalculate_weights),
+        y=fdk_weigh_projections(A, y, src_rot_center_dist),
         padded=padded,
         filter=filter,
         reject_acyclic_filter=reject_acyclic_filter
